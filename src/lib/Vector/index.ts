c6480c23aa3328b5175111e40d9d6d5234a91116
yashweblife <yashweblife20@gmail.com>
2025-01-25 14:48:32 -0600

    add initial vector code

--- src/lib/Vector/index.ts
+++ src/lib/Vector/index.ts
@@ -1,1 +1,32 @@
-export default class Vector { }
+export default class Vector {
+    
+    constructor(public x:number=0,public y:number=0,public z:number=0){}
+    add(v:Vector){
+        this.x += v.x
+        this.y += v.y
+        this.z += v.z
+    }
+    sub(v:Vector){
+        this.x -= v.x
+        this.y -= v.y
+        this.z -= v.z
+    }
+    mag(){
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z)
+    }
+    normalize(){
+        const m = this.mag()
+        this.x /= m
+        this.y /= m
+        this.z /= m
+    }
+    scale(s:number){
+        this.normalize()
+        this.x *= s
+        this.y *= s
+        this.z *= s
+    }
+    distance(v:Vector){
+        return Math.sqrt((v.x - this.x) * (v.x - this.x) + (v.y - this.y) * (v.y - this.y) + (v.z - this.z) * (v.z - this.z))
+    }
+}
