c8000062927e64048a04ea34ab215c2f60106251
yashweblife <yashweblife20@gmail.com>
2025-01-28 17:35:39 -0600

    add canvas to body

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Canvas from "./lib/Canvas";
 import FallAndBounce from "./Simulations/FallAndBounce";
 
-const canvas = new Canvas(document.getElementById("app") as HTMLElement)
-canvas.setSize(500,500)
+const canvas = new Canvas(document.body as HTMLElement)
+canvas.setSize(window.innerWidth, window.innerHeight)
 
 const fallAndBounce = new FallAndBounce(canvas)
 function animation(){
