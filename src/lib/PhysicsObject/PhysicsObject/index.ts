aed014195284b083d053c885fae676f776602a15
yashweblife <yashweblife20@gmail.com>
2025-01-30 13:02:07 -0600

    update contents of PO lib

--- src/lib/PhysicsObject/PhysicsObject/index.ts
+++ src/lib/PhysicsObject/PhysicsObject/index.ts
@@ -4,5 +4,11 @@ export default class PhysicsObject {
     pos:Vector=new Vector()
     vel:Vector=new Vector()
     mass:number=1
-    
+    constructor(){}
+    attract(obj:PhysicsObject, mag:number=1){}
+    repel(obj:PhysicsObject, mag:number=1){}
+    bound(x:number,y:number,width:number,height:number){}
+    addForce(f:Vector){}
+    updateMotion(){}
+    static Gravity:Vector=new Vector(0,0.1)
 }
