881b21148c0605d80f59a42b3b631d1dc820eabd
yashweblife <yashweblife20@gmail.com>
2025-01-30 10:54:11 -0600

    create a manager for sims

--- src/Simulations/index.ts
+++ src/Simulations/index.ts
@@ -1,7 +1,17 @@
 import Attraction from "./Attraction";
 import FallAndBounce from "./FallAndBounce";
 
-export default {
-    FallAndBounce,
-    Attraction
-}
+
+const list_of_simulations = [
+    {
+        name: "Fall and Bounce",
+        info:"",
+        world: FallAndBounce
+    },{
+        name: "Attraction",
+        info:"",
+        world: Attraction
+    }
+]
+
+export default list_of_simulations
