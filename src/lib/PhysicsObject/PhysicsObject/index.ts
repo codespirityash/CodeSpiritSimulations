003371c93be49f48259ec6fce8e34796f91c25df
yashweblife <yashweblife20@gmail.com>
2025-01-30 13:15:34 -0600

    add comments

--- src/lib/PhysicsObject/PhysicsObject/index.ts
+++ src/lib/PhysicsObject/PhysicsObject/index.ts
@@ -5,18 +5,61 @@ export default class PhysicsObject {
     vel:Vector=new Vector()
     mass:number=1
     constructor(){}
+    /**
+     * uses Newton's Law of Gravitation
+     * 
+     * forceMag = G * m1 * m2 / r^2
+     * AforceDir = A-B
+     * BforceDir = B-A
+     * A(force) = AforceDir * forceMag
+     * B(force) = BforceDir * forceMag
+     * 
+     * @param obj PhysicsObject
+     * @param mag number to scale the force
+     * @returns this
+     */
     attract(obj:PhysicsObject, mag:number=1){
         return this
     }
+    /**
+     * uses Newton's Law of Gravitation
+     * 
+     * forceMag = -G * m1 * m2 / r^2
+     * AforceDir = A-B
+     * BforceDir = B-A
+     * A(force) = AforceDir * forceMag
+     * B(force) = BforceDir * forceMag
+     * 
+     * @param obj PhysicsObject
+     * @param mag number to scale the force
+     * @returns this
+     */
     repel(obj:PhysicsObject, mag:number=1){
         return this
     }
+    /**
+     * Reverses velocity(axis) if position is out of bounds
+     * @param x min x
+     * @param y min y
+     * @param width max x
+     * @param height max y
+     * @returns 
+     */
     bound(x:number,y:number,width:number,height:number){
         return this
     }
+    /**
+     * Adds a force to the objects velocity
+     * @param f Vector
+     * @returns 
+     */
     addForce(f:Vector){
         return this
     }
+    /**
+     * Adds updated velocity to the objects position
+     * @returns 
+     */
     updateMotion(){
         return this
     }
