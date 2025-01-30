d3caf35dd50be6fc3e2d5132e246092108c2d646
yashweblife <yashweblife20@gmail.com>
2025-01-30 13:06:52 -0600

    rework ball to better fit use cases

--- src/lib/PhysicsObject/Ball/index.ts
+++ src/lib/PhysicsObject/Ball/index.ts
@@ -1,56 +1,39 @@
 import Canvas from "../../Canvas"
 import Vector from "../../Vector"
+import PhysicsObject from "../PhysicsObject"
 
-export default class Ball {
-    public constructor(
-        public pos: Vector = new Vector(),
-        public vel: Vector = new Vector(),
-        public mass: number = 1,
-        public radius: number = 10,
-        public color: string = 'red',
-    ) {
+export default class Ball extends PhysicsObject {
+    radius: number=1
+    color: string="red"
+
+    constructor() {
+        super()
+    }
+    setPos(pos: Vector) {
         this.pos = pos
+        return this
+    }
+    setVel(vel: Vector) {
         this.vel = vel
-        this.mass = mass
+        return this
+    }
+    setRadius(radius: number) {
         this.radius = radius
+        return this
+    }
+    setColor(color: string) {
         this.color = color
+        return this
     }
     draw(canvas: Canvas, fill: boolean = false) {
         canvas.arc(this.pos, this.radius, this.color, fill)
         return this
     }
-    bound(x: number, y: number, width: number, height: number) {
-        if (this.pos.x + this.radius > width) {
-            this.pos.x = width - this.radius
-            this.vel.x *= -1
-        } else if (this.pos.x - this.radius < x) {
-            this.pos.x = x + this.radius
-            this.vel.x *= -1
-        }
-        if (this.pos.y + this.radius > height) {
-            this.pos.y = height - this.radius
-            this.vel.y *= -1
-        } else if (this.pos.y - this.radius < y) {
-            this.pos.y = y + this.radius
-            this.vel.y *= -1
-        }
-        return this
-    }
-    addForce(f: Vector) {
-        this.vel.add(f)
-        return this
-    }
     move() {
-        this.pos.add(this.vel)
+        this.updateMotion()
         return this
     }
     static randomBall(width: number, height: number) {
-        return new Ball(
-            new Vector(Math.floor(Math.random() * width), Math.floor(Math.random() * height)),
-            new Vector(0,0,0),
-            1,
-            Math.random() * 10 + 5,
-            "red"
-        )
+        return new Ball()
     }
 }
