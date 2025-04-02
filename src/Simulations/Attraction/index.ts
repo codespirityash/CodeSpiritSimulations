59eed14217bbd34742780e622f4fcb25e9a7edad
codespirityash <codespirityash@gmail.com>
2025-04-02 13:57:23 +0530

    add attraction logiv

--- src/Simulations/Attraction/index.ts
+++ src/Simulations/Attraction/index.ts
@@ -1,5 +1,18 @@
+import Canvas from "../../lib/Canvas";
+import Ball from "../../lib/PhysicsObject/Ball";
+import Vector from "../../lib/Vector";
+
 export default class Attraction {
-    constructor(){
+    agents: Ball[] = [];
+    attractor: Ball;
+    repulsor: Ball;
+
+    constructor(public canvas: Canvas) {
+        this.agents = new Array(50).fill(0).map(() => Ball.randomBall(canvas.size.width, canvas.size.height));
+        this.attractor = new Ball().setPos(new Vector(canvas.size.width / 4, canvas.size.height / 2)).setRadius(10).setColor("green");
+        this.repulsor = new Ball().setPos(new Vector(3 * canvas.size.width / 4, canvas.size.height / 2)).setRadius(10).setColor("red");
     }
-    animate(){}
+
+    animate() {
+    }
 }
